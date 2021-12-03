05721b859eac8bfbddd525a14ec3a5a853ce58bf
kukinpower <thekukinpower@gmail.com>
2021-12-03 20:14:10 +0300

    add user model

--- ex00/Cinema/src/main/java/edu/school21/cinema/models/CinemaUser.java
+++ ex00/Cinema/src/main/java/edu/school21/cinema/models/CinemaUser.java
@@ -1,5 +1,17 @@
 package edu.school21.cinema.models;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class CinemaUser {
 
+  private String firstName;
+  private String lastName;
+  private String phoneNumber;
+  private String password;
 }
